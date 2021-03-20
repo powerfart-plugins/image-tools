229222a9e64b806a80d9534689bcf3539d4b4672
Xinos <userxinos@gmail.com>
2021-03-20 15:44:10 +0700

    fix a random client crash

--- components/ImageModalFooter.jsx
+++ components/ImageModalFooter.jsx
@@ -5,8 +5,6 @@ const { zipUrl } = require('../utils');
 
 const { downloadLink } = getModule([ 'downloadLink' ], false);
 const { footer } = getModule([ 'avatarHint', 'footer' ], false);
-const Retry = getModuleByDisplayName('Retry', false);
-const Dropper = getModuleByDisplayName('Dropper', false);
 // const { CarouselPrevious, CarouselNext } = getModule([ 'CarouselPrevious', 'CarouselNext' ], false);
 
 module.exports = class ImageFooter extends React.PureComponent {
@@ -18,6 +16,8 @@ module.exports = class ImageFooter extends React.PureComponent {
   render () {
     const { getData, children } = this.props;
     const { resolution, size, name, url } = getData();
+    const Retry = getModuleByDisplayName('Retry', false); // в render, тк высока вероятность получить null
+    const Dropper = getModuleByDisplayName('Dropper', false);
 
     return (
       <div className={`image-tools-modal-footer ${footer} ${downloadLink}`}>
