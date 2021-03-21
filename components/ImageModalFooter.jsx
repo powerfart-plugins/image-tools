d8c27735961cf77d42e148f1b802ea2f774928d4
Xinos <userxinos@gmail.com>
2021-03-21 20:44:07 +0700

    WIP new footer design (Thanks to the community for the feedback)

--- components/ImageModalFooter.jsx
+++ components/ImageModalFooter.jsx
@@ -1,10 +1,9 @@
-const { React, getModule, getModuleByDisplayName } = require('powercord/webpack');
+const { React, getModule } = require('powercord/webpack');
 
 const Copy = require('./Copyable.jsx');
 const { zipUrl } = require('../utils');
 
 const { downloadLink } = getModule([ 'downloadLink' ], false);
-const { footer } = getModule([ 'avatarHint', 'footer' ], false);
 // const { CarouselPrevious, CarouselNext } = getModule([ 'CarouselPrevious', 'CarouselNext' ], false);
 
 module.exports = class ImageFooter extends React.PureComponent {
@@ -16,27 +15,14 @@ module.exports = class ImageFooter extends React.PureComponent {
   render () {
     const { getData, children } = this.props;
     const { resolution, size, name, url } = getData();
-    const Retry = getModuleByDisplayName('Retry', false); // в render, тк высока вероятность получить null
-    const Dropper = getModuleByDisplayName('Dropper', false);
+    // const Retry = getModuleByDisplayName('Retry', false); // в render, тк высока вероятность получить null
+    // const Dropper = getModuleByDisplayName('Dropper', false);
 
     return (
-      <div className={`image-tools-modal-footer ${footer} ${downloadLink}`}>
+      <div className={`image-tools-modal-footer ${downloadLink}`}>
         <div className='content'>
 
           { children }
-
-          <div className='buttons'>
-            <div>
-              <div/>
-              <div/>
-              <div/>
-            </div>
-            <div>
-              <div><Retry width="20" height="20"/></div>
-              <div><Dropper width="20" height="20" /></div>
-              <div/>
-            </div>
-          </div>
 
           <div className='image-info'>
             { (resolution && size && name && url) && <>
